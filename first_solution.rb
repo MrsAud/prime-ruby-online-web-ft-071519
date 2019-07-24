b54ab4522e40490fbb6b547c394d92bc63a97927
Audrea Cook <audreacwork@gmail.com>
2019-07-24 16:09:55 +0000

    Automatically backed up by Learn

---
+++ first_solution.rb
@@ -0,0 +1,16 @@
+def prime?(num)
+  i = 1
+  if num <= 1
+    return false
+  end
+  
+  while i < num do
+    if (num % i == 0) && i != 1 && i != num && num 
+      return false
+    else
+      i += 1
+    end
+  end
+  
+  true
+end
