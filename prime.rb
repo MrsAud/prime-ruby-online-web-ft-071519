9cd73ca84bd696ae31153b0be14f28580979d63d
Audrea Cook <audreacwork@gmail.com>
2019-07-24 16:37:21 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -2,10 +2,10 @@ def prime?(num)
   i = 1
   while i < num do
     if num <= 1 && num % i == 0 && i != 1 && i != num
-      return false
+      return true
     else
       i += 1
     end
   end
-  true
+  false
 end
