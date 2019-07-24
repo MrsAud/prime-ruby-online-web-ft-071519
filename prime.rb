b43903185782a4f798d7c8c9caf5a3aec4c4c54a
Audrea Cook <audreacwork@gmail.com>
2019-07-24 16:10:01 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,17 +1,1 @@
-def prime?(num)
-  i = 1
-  if num <= 1
-    return false
-  end
-  
-  while i < num do
-    if (num % i == 0) && i != 1 && i != num && num 
-      return false
-    else
-      i += 1
-    end
-  end
-  
-  true
-end
 
